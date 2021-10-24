f7435f2c8523e6ef3a6114c610f2c3f84b23b982
NikitaVisheratin <visheratinn@gmail.com>
2021-10-24 19:09:14 +0300

    first commit

---
+++ Work_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+    int arr[10];
+    for (int* p = arr; p < arr + 10; ++p) {
+        *p = (p - arr) + 1; //заполнили числами от 1 до 10
+    }
+    std::cout << "Array:\n";
+    for (int* p = arr; p < arr + 10; ++p) {
+        std::cout << "[" << * p << "] ";
+    }
+    std::cout << std::endl;
+    std::string str = "String";
+    std::cout << "String:\n";
+    for (char *c = &str[0]; c < &str[0] + str.length(); ++c) {
+        std::cout << *c << " ";
+    }
+}
